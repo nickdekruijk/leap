af1ba7bc6b6c5417263ce7b396a833861ddf3e14
Nick de Kruijk <git@nickdekruijk.nl>
2023-07-25 17:50:55 +0200

    Refactor

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
+use NickDeKruijk\Leap\Controllers\AssetController;
 Route::group(['middleware' => ['web']], function () {
     // Assets, this way we don't need to publish them to public
-    Route::get(config('leap.route_prefix') . '/leap.css', 'NickDeKruijk\Leap\Controllers\AssetController@css')->name('leap.css');
+    Route::get(config('leap.route_prefix') . '/leap.css', [AssetController::class, 'css'])->name('leap.css');
 });
