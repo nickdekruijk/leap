654f4fe0189bf2b27d1338473f820dc562e4ceaa
Nick de Kruijk <git@nickdekruijk.nl>
2023-08-01 15:26:03 +0200

    Refactor routes

--- src/routes.php
+++ src/routes.php
@@ -6,21 +6,19 @@ use NickDeKruijk\Leap\Livewire\Dashboard;
 use NickDeKruijk\Leap\Livewire\Login;
 use NickDeKruijk\Leap\Middleware\Leap;
 
-// Set login and logout routes if required
-if (config('leap.auth_routes', true)) {
-    Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => ['web']], function () {
+    // Assets, this way we don't need to publish them to public
+    Route::get(config('leap.route_prefix') . '/leap.css', [AssetController::class, 'css'])->name('leap.css');
+
+    // Set login and logout routes if required
+    if (config('leap.auth_routes')) {
         Route::get(config('leap.route_prefix') . '/login',  Login::class)->name('leap.login');
         Route::post(config('leap.route_prefix') . '/logout', function () {
             Auth::logout();
             request()->session()->regenerateToken();
             return redirect()->route('leap.login');
         })->name('leap.logout');
-    });
-}
-
-Route::group(['middleware' => ['web']], function () {
-    // Assets, this way we don't need to publish them to public
-    Route::get(config('leap.route_prefix') . '/leap.css', [AssetController::class, 'css'])->name('leap.css');
+    }
 });
 
 Route::group(['middleware' => ['web', Leap::class]], function () {
