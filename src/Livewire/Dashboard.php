d35f664f42e53f8dd55adca586b954342f8d0c74
Nick de Kruijk <git@nickdekruijk.nl>
2023-08-01 17:23:10 +0200

    Dashboard guard

--- src/Livewire/Dashboard.php
+++ src/Livewire/Dashboard.php
@@ -2,10 +2,16 @@
 
 namespace NickDeKruijk\Leap\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public function __construct()
+    {
+        Auth::shouldUse(config('leap.guard'));
+    }
+
     public function render()
     {
         return view('leap::livewire.dashboard')->layout('leap::layouts.app');
