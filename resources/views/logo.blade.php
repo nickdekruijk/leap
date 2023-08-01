49964df0939478ad587368153d1d2a9160c4f9e8
Nick de Kruijk <git@nickdekruijk.nl>
2023-08-01 15:25:29 +0200

    Use app name as logo

--- resources/views/logo.blade.php
+++ resources/views/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="logo">
-    <a href="/" target="_blank">@svg('fab-laravel', 'svg')Leap</a>
+    <a href="/" target="_blank">@svg('fab-laravel', 'svg'){{ config('app.name') }}</a>
 </div>
