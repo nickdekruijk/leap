05dba113acaeb2e61d7752a1cd3695c3f98fd9e7
Nick de Kruijk <git@nickdekruijk.nl>
2023-08-01 15:59:10 +0200

    Update dashboard.blade.php

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,3 +1,4 @@
 <div>
-    <h2>@lang('welcome') {{ auth()->user()->name }}</h2>
+    <h1 class="header">{{ request()->segment(2) ?: 'Dashboard' }}</h1>
+    <h2>@lang('welcome') {{ auth(config('leap.guard'))->user()->name }}</h2>
 </div>
