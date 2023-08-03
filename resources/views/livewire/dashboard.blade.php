900900276e005f04e6cdc54e71f0dcbc8036e44f
Nick de Kruijk <git@nickdekruijk.nl>
2023-08-03 17:57:13 +0200

    Dashboard greeting

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,15 @@
 <div>
     <h1 class="header">@lang($title)</h1>
-    <h2>@lang('welcome') {{ auth(config('leap.guard'))->user()->name }}</h2>
+    <div class="dashboard">
+        <h2><span x-init="$el.innerHTML=greeting()"></span> {{ auth(config('leap.guard'))->user()->name }}</h2>
+        <p>Your are logged in as a {{ request()->get('leap_role')->name }}.</p>
+    </div>
+    <script>
+        function greeting() {
+            const hour = new Date().getHours();
+            if (hour < 12) return '@lang('good_morning')'
+            else if (hour < 18) return '@lang('good_afternoon')'
+            else return '@lang('good_evening')'
+        }
+    </script>
 </div>
