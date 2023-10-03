b0b903bee263e38ac5fbfa50f7670614f3eed8fd
Nick de Kruijk <git@nickdekruijk.nl>
2023-10-03 16:08:45 +0200

    Improved Dashboard greeting

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,15 +1,17 @@
 <div>
     <h1 class="header">@lang($title)</h1>
     <div class="dashboard">
-        <h2><span x-init="$el.innerHTML=greeting()"></span> {{ auth(config('leap.guard'))->user()->name }}</h2>
+        <h2 x-init="$el.innerHTML = greeting()">&nbsp;</h2>
         <p>Your are logged in as a {{ session('leap.role')->name }}.</p>
     </div>
     <script>
         function greeting() {
             const hour = new Date().getHours();
-            if (hour < 12) return '@lang('good_morning')'
-            else if (hour < 18) return '@lang('good_afternoon')'
-            else return '@lang('good_evening')'
+            let greeting = '';
+            if (hour < 12) greeting = '@lang('good_morning')'
+            else if (hour < 18) greeting = '@lang('good_afternoon')'
+            else greeting = '@lang('good_evening')';
+            return greeting + ' {{ auth(config('leap.guard'))->user()->name }}';
         }
     </script>
 </div>
