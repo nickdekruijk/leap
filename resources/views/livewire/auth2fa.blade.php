7c45645cd9da9c5dd0fd6d423fe09ad6af5ce0cb
Nick de Kruijk <git@nickdekruijk.nl>
2024-02-13 15:29:21 +0100

    Update 2FA form

--- resources/views/livewire/auth2fa.blade.php
+++ resources/views/livewire/auth2fa.blade.php
@@ -1,27 +1,29 @@
-<div class="login">
-    <div class="login-popup">
-        @include('leap::logo')
-        <form wire:submit="submit" class="form">
-            @if ($message)
-                <div class="form-message">
-                    {!! $message !!}
-                </div>
-            @endif
-            <label>
-                @lang('verification_code')
-                <input class="input" size="30" type="text" name="code" wire:model="code" autofocus autocomplete="one-time-code">
-            </label>
-            @if ($errors->any())
-                <div class="form-errors">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <button type="submit">@svg('fas-sign-in-alt', 'svg') @lang('login')</button>
-            <button wire:click="logout">@svg('fas-sign-out-alt', 'svg')@lang('logout')</button>
-        </form>
-    </div>
-</div>
+<main class="container login login-2fa">
+    <article class="grid">
+        <div>
+            @include('leap::logo')
+
+            <form wire:submit="submit" class="form" novalidate>
+                @if ($message)
+                    <div class="form-message">
+                        {!! $message !!}
+                    </div>
+                @endif
+
+                <label>
+                    @lang('verification_code')
+                    @error('code')
+                        <span class="form-error">{{ $message }}</span>
+                    @enderror
+                    <input size="{{ config('leap.auth_2fa.mail.code.length') }}" 
+                        @error('code') aria-errormessage="{{ $message }}" aria-invalid="true" @enderror
+                        aria-label="@lang('verification_code')"
+                        type="text" name="code" wire:model.live="code" autofocus autocomplete="one-time-code">
+                </label>
+
+                <button type="submit">@svg('fas-sign-in-alt', 'button-svg') @lang('login')</button>
+                <button wire:click="logout">@svg('fas-sign-out-alt', 'button-svg') @lang('logout')</button>
+            </form>
+        </div>
+    </article>
+</main>
