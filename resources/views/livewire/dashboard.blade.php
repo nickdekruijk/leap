2dd282b7170addc6db723de70d6b021afbea1625
Nick de Kruijk <git@nickdekruijk.nl>
2024-02-13 18:02:11 +0100

    Update dashboard.blade.php

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,19 +1,24 @@
 <div class="dashboard">
-    <header><h2>{{ $this->getTitle() }}</h2></header>
-    <hgroup>
-        <h3 x-init="$el.innerHTML = greeting()">&nbsp;</h3>
-        <h4>
-            Your are logged in as {{ session('leap.role')->name }}{{ config('leap.organizations') ? ' for '.session('leap.role.organization.name') : '' }}.
-        </h4>
-    </hgroup>
-    <script>
-        function greeting() {
-            const hour = new Date().getHours();
-            let greeting = '';
-            if (hour < 12) greeting = '@lang('good_morning')'
-            else if (hour < 18) greeting = '@lang('good_afternoon')'
-            else greeting = '@lang('good_evening')';
-            return greeting + ' {{ auth(config('leap.guard'))->user()->name }}';
-        }
-    </script>
+    <header>
+        <h2>{{ $this->getTitle() }}</h2>
+    </header>
+    <article>
+        <hgroup>
+            <h3 x-init="$el.innerHTML = greeting()">&nbsp;</h3>
+            <h4>
+                Your are logged in as
+                {{ session('leap.role')->name }}{{ config('leap.organizations') ? ' for ' . session('leap.role.organization.name') : '' }}.
+            </h4>
+        </hgroup>
+        <script>
+            function greeting() {
+                const hour = new Date().getHours();
+                let greeting = '';
+                if (hour < 12) greeting = '@lang('good_morning')'
+                else if (hour < 18) greeting = '@lang('good_afternoon')'
+                else greeting = '@lang('good_evening')';
+                return greeting + ' {{ auth(config('leap.guard'))->user()->name }}';
+            }
+        </script>
+    </article>
 </div>
