578e2596a1c5d0de2e4efbf3caa2cfe07b47dc0a
Nick de Kruijk <git@nickdekruijk.nl>
2024-03-28 15:57:20 +0100

    Add href option for buttons

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,10 @@
-@props(['label', 'svgIcon' => null, 'class' => ''])
-<button class="leap-button {{ $class }}" {{ $attributes }}>
-    <x-leap::icon />@lang($label)
-</button>
+@props(['label', 'svgIcon' => null, 'class' => '', 'href' => null])
+@if ($href)
+    <a class="leap-button {{ $class }}" href="{{ $href }}" wire:navigate>
+        <x-leap::icon />@lang($label)
+    </a>
+@else
+    <button class="leap-button {{ $class }}" {{ $attributes }}>
+        <x-leap::icon />@lang($label)
+    </button>
+@endif
