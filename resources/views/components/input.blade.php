9186c92b746476d6e58490c67b6b02cc9d2ee993
Nick de Kruijk <git@nickdekruijk.nl>
2024-04-01 11:44:11 +0200

    Update inputs disabled logic

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -9,7 +9,9 @@
     <input size="30" class="leap-input {{ $class }}"
         @error($name) aria-errormessage="{{ $message }}" aria-invalid="true" @elseif (isset($$name) && $name != 'password') aria-invalid="false" @enderror
         id="{{ $name }}"
-        @if (Gate::denies('leap::create') && Gate::denies('leap::update')) disabled @endif
+        @auth(config('leap.guard'))
+            @if (Gate::denies('leap::create') && Gate::denies('leap::update')) disabled @endif
+        @endauth
         aria-label="@lang($label ?: $placeholder ?? '')"
         wire:model{{ isset($wire) ? '.' . $wire : '' }}="{{ $name }}"
         {{ $attributes }}>
--- resources/views/components/switch.blade.php
+++ resources/views/components/switch.blade.php
@@ -6,6 +6,9 @@
     <input class="leap-input {{ $class }}" type="checkbox" role="switch" 
         @error($name) aria-errormessage="{{ $message }}" aria-invalid="true" @elseif (isset($$name) && $name != 'password') aria-invalid="false" @enderror
         id="{{ $name }}"
+        @auth(config('leap.guard'))
+            @if (Gate::denies('leap::create') && Gate::denies('leap::update')) disabled @endif
+        @endauth
         wire:model{{ isset($wire) ? '.' . $wire : '' }}="{{ $name }}"
         aria-label="@lang($label)"
         {{ $attributes }}><span class="leap-label">@lang($label)</span>
