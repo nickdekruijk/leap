dd628d478c379adb71c20b7e4f35561b5ba421cb
Nick de Kruijk <git@nickdekruijk.nl>
2024-05-29 18:47:22 +0200

    Formatting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Leap</title>
-    {!! \NickDeKruijk\Leap\Controllers\AssetController::cssLink() !!}
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Leap</title>
+        {!! \NickDeKruijk\Leap\Controllers\AssetController::cssLink() !!}
+    </head>
 
-<body>
-    <div class="leap">
-        @auth(config('leap.guard'))
-            @if (!NickDeKruijk\Leap\Controllers\Auth2FAController::mustValidate())
-                @livewire('leap.navigation')
-            @endif
-        @endauth
-        @livewire('leap.toasts')
-        {{ $slot }}
-    </div>
-</body>
+    <body>
+        <div class="leap">
+            @auth(config('leap.guard'))
+                @if (!NickDeKruijk\Leap\Controllers\Auth2FAController::mustValidate())
+                    @livewire('leap.navigation')
+                @endif
+            @endauth
+            @livewire('leap.toasts')
+            {{ $slot }}
+        </div>
+    </body>
 
 </html>
