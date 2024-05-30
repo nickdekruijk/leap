791c44536d6b36f3e444b18e5df2ad9dc1deff77
Nick de Kruijk <git@nickdekruijk.nl>
2024-05-30 14:27:55 +0200

    Add clone/delete buttons

--- resources/views/livewire/editor.blade.php
+++ resources/views/livewire/editor.blade.php
@@ -3,6 +3,12 @@
         @can('leap::update')
             <x-leap::button svg-icon="far-save" wire:click="submit" label="save" wire:loading.delay.shorter.attr="disabled" class="primary" type="submit" />
         @endcan
+        @can('leap::create')
+            <x-leap::button svg-icon="far-copy" wire:click="clone" label="save-copy" wire:loading.delay.shorter.attr="disabled" />
+        @endcan
+        @can('leap::delete')
+            <x-leap::button svg-icon="far-trash-alt" wire:click="delete" label="delete" wire:loading.delay.shorter.attr="disabled" class="secondary" />
+        @endcan
         <x-leap::button svg-icon="fas-xmark" x-on:click="active=false" wire:click="close()" label="cancel" />
     </div>
     <form class="leap-form" wire:submit="submit">
