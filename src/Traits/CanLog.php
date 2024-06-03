5d72175e5def11062a8e52dd622426d887dc4eba
Nick de Kruijk <git@nickdekruijk.nl>
2024-06-03 15:33:50 +0200

    Context can be string, use get_called_class for module

--- src/Traits/CanLog.php
+++ src/Traits/CanLog.php
@@ -13,12 +13,19 @@ trait CanLog
      * Also checks if the action is allowed to be logged and if the ip address should be anonymized
      *
      * @param string $action Action to log, e.g. login, create, update, delete
-     * @param array $context Context of the action, e.g. email address when trying to login
+     * @param array|string $context Context of the action, e.g. email address when trying to login
      * @return Log|null
      */
-    public function log(string $action, array $context = []): ?Log
+    public function log(string $action, array|string $context = []): ?Log
     {
-        if (!in_array($action, config('leap.logging.skip_actions')) && !in_array(self::class, config('leap.logging.skip_modules'))) {
+        $module = get_called_class();
+
+        // If context is a string convert it to an array
+        if (is_string($context)) {
+            $context = [$context];
+        }
+
+        if (!in_array($action, config('leap.logging.skip_actions')) && !in_array($module, config('leap.logging.skip_modules'))) {
             // Anonymize IP address if needed
             $ip = config('leap.logging.ip_address_anonymized') ? preg_replace(['/\.\d*$/', '/[\da-f]*:[\da-f]*$/'], ['.xxx', 'xxxx:xxxx'], request()->ip()) : request()->ip();
 
@@ -26,7 +33,7 @@ trait CanLog
             return Log::create([
                 'ip' => config('leap.logging.ip_address') ? $ip : null,
                 'user_agent' => config('leap.logging.user_agent') ? request()->userAgent() : null,
-                'module' => self::class,
+                'module' => $module,
                 'action' => $action,
                 'context' => $context ?: null,
                 'user_id' => auth()->id(),
