08618b90daaab0120b3ae64678df7b25dc4e51ce
Nick de Kruijk <git@nickdekruijk.nl>
2024-06-05 15:38:02 +0200

    Fix profile editing

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,9 @@
-@props(['attribute', 'placeholder'])
+@props(['attribute', 'placeholder', 'name'])
 
 <x-leap::label>
     <input class="leap-input"
+        @error($attribute->dataName ?? $name) aria-errormessage="{{ $message }}" aria-invalid="true" @enderror
         @isset($attribute)
-            @error($attribute->dataName) aria-errormessage="{{ $message }}" aria-invalid="true" @enderror
             placeholder="{{ $placeholder[$attribute->name] ?? $attribute->placeholder }}"
             @if (auth(config('leap.guard'))->user() && Gate::denies('leap::create') && Gate::denies('leap::update')) disabled @endif
             aria-label="@lang($attribute->label)"
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -12,16 +12,16 @@
         <form class="leap-form" wire:submit="submit">
             <fieldset class="leap-fieldset">
                 <h3>@lang('profile_edit')</h3>
-                <x-leap::input wire="blur" name="data.name" label="name" type="text" autocomplete="name" />
-                <x-leap::input wire="blur" name="data.email" label="email" type="email" disabled />
+                <x-leap::input wire:model.blur="data.name" name="data.name" label="name" autocomplete="name" />
+                <x-leap::input wire:model.blur="data.email" name="data.email" label="email" type="email" disabled />
             </fieldset>
         </form>
         <form class="leap-form" wire:submit="submit">
             <fieldset class="leap-fieldset">
                 <h3>@lang('update_password')</h3>
-                <x-leap::input wire="blur" name="data.password_current" label="password_current" type="password" autocomplete="current-password" />
-                <x-leap::input wire="blur" name="data.password_new" label="password_new" type="password" autocomplete="new-password" />
-                <x-leap::input wire="blur" name="data.password_new_confirmation" label="password_new_confirmation" type="password" autocomplete="new-password" />
+                <x-leap::input wire:model.blur="data.password_current" name="data.password_current" label="password_current" type="password" autocomplete="current-password" />
+                <x-leap::input wire:model.blur="data.password_new" name="data.password_new" label="password_new" type="password" autocomplete="new-password" />
+                <x-leap::input wire:model.blur="data.password_new_confirmation" name="data.password_new_confirmation" label="password_new_confirmation" type="password" autocomplete="new-password" />
             </fieldset>
         </form>
     </div>
