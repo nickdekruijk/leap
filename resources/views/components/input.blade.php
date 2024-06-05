eb89042d7814250e52c16db97ade5c180d81ea90
Nick de Kruijk <git@nickdekruijk.nl>
2024-06-05 14:00:46 +0200

    Remove label conditionals since it's always set

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -5,6 +5,6 @@
         @error($attribute->dataName) aria-errormessage="{{ $message }}" aria-invalid="true" @enderror
         placeholder="{{ $placeholder[$attribute->name] ?? $attribute->placeholder }}"
         @if (auth(config('leap.guard'))->user() && Gate::denies('leap::create') && Gate::denies('leap::update')) disabled @endif
-        aria-label="@lang($attribute->label ?: $placeholder)"
+        aria-label="@lang($attribute->label)"
         wire:model{{ isset($attribute->wire) ? '.' . $attribute->wire : '' }}="{{ $attribute->dataName }}" {{ $attributes }}>
 </x-leap::label>
--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,8 +1,6 @@
 @aware(['attribute'])
 <label class="leap-label">
-    @if ($attribute->label)
-        <span class="leap-label">@lang($attribute->label)</span>
-    @endif
+    <span class="leap-label">@lang($attribute->label)</span>
     @error($attribute->dataName)
         <span class="leap-error">{{ $message }}</span>
     @enderror
