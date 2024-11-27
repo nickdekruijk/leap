1a70b5656344b3b1aef2af0c7d4e8a34c7fe65d8
Nick de Kruijk <git@nickdekruijk.nl>
2024-11-27 13:58:01 +0100

    Make items buttons in index for keyboard accessibility

--- resources/views/livewire/resource.blade.php
+++ resources/views/livewire/resource.blade.php
@@ -7,7 +7,7 @@
         <table class="leap-index-table">
             <tr class="leap-index-header">
                 @foreach ($this->indexAttributes() as $attribute)
-                    <th wire:click="order('{{ $attribute->name }}')" class="{{ $this->orderBy === $attribute->name ? ($this->orderDesc ? 'order-desc' : 'order-asc') : '' }}">{{ $attribute->labelIndex }}</th>
+                    <th class="{{ $this->orderBy === $attribute->name ? ($this->orderDesc ? 'order-desc' : 'order-asc') : '' }}"><button class="button-link" wire:click="order('{{ $attribute->name }}')">{{ $attribute->labelIndex }}</button></th>
                 @endforeach
             </tr>
             @foreach ($this->indexRows() as $row)
@@ -21,10 +21,16 @@
                 <tr x-on:click="$dispatch('openEditor',{id:(selectedRow={{ $row['id'] }})})" x-bind:class="selectedRow == {{ $row['id'] }} ? 'leap-index-row-selected' : ''" class="leap-index-row">
                     @foreach ($this->indexAttributes() as $attribute)
                         <td>
+                            @if ($loop->first)
+                                <button class="button-link" x-on:click="$dispatch('openEditor',{id:(selectedRow={{ $row['id'] }})})">
+                            @endif
                             @if ($attribute->type == 'checkbox')
                                 <span class="leap-row-checkbox leap-row-checkbox-{{ $row[$attribute->name] ? 'checked' : 'unchecked' }}"></span>
                             @else
                                 {{ $row[$attribute->name] }}
+                            @endif
+                            @if ($loop->first)
+                                </button>
                             @endif
                         </td>
                     @endforeach
