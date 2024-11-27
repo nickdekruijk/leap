743e90e9926ea9cde648ccec74a86288441a6b84
Nick de Kruijk <git@nickdekruijk.nl>
2024-11-27 14:16:46 +0100

    Logo not tab accessible

--- resources/views/logo.blade.php
+++ resources/views/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="leap-logo">
-    <a href="/" target="_blank">@svg('fab-laravel', 'svg')<span>{{ config('app.name') }}</span></a>
+    <a tabindex="-1" href="/" target="_blank">@svg('fab-laravel', 'svg')<span>{{ config('app.name') }}</span></a>
 </div>
