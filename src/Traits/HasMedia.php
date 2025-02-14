252328f53ea087c3981901153c757a157850ef6b
Nick de Kruijk <git@nickdekruijk.nl>
2025-02-14 16:59:30 +0100

    Create HasMedia.php

---
+++ src/Traits/HasMedia.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NickDeKruijk\Leap\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use NickDeKruijk\Leap\Models\Media;
+
+trait HasMedia
+{
+    public function media(): MorphToMany
+    {
+        return $this->morphToMany(Media::class, 'mediable', config('leap.table_prefix') . 'mediables');
+    }
+}
