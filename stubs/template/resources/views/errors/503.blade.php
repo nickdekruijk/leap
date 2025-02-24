249cf3274f3bec163af10aa520fe24d6f19b2802
Nick de Kruijk <git@nickdekruijk.nl>
2025-02-24 15:56:47 +0100

    Create 503.blade.php

---
+++ stubs/template/resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        <a name="main"></a>
+        <div class="main-width">
+            <article class="article">
+                <br><br><br>
+                <h1>Onderhoud</h1>
+                <p>We zijn tijdelijk niet bereikbaar vanwege werkzaamheden aan de website. Probeer het later nog eens.</p>
+            </article>
+        </div>
+    </main>
+@endsection
