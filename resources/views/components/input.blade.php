106d2a2fea274b3469cc463cc83520d9faf680af
Nick de Kruijk <git@nickdekruijk.nl>
2025-08-08 13:46:05 +0200

    Fix error directive to handle null input name
    Updated the @error directive in the input component to safely handle cases where the input name may be null, preventing potential errors when rendering the component.

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,7 +3,7 @@
 <x-leap::label>
     <input class="leap-input"
         @if (($attribute->type ?? false) == 'password') autocomplete="new-password" @endif
-        @error($attribute->dataName ?? $name) aria-errormessage="{{ $message }}" aria-invalid="true" @enderror
+        @error($attribute->dataName ?? ($name ?? '')) aria-errormessage="{{ $message }}" aria-invalid="true" @enderror
         @if ($attribute->disabled ?? false || (auth(config('leap.guard'))->user() && Gate::denies('leap::create') && Gate::denies('leap::update'))) disabled @endif
         @isset($attribute)
             placeholder="{{ $placeholder[$attribute->name] ?? $attribute->placeholder }}"
