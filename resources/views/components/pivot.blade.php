26a6ef7d0dc04a074641ff07c687e9d0fe920c0d
Nick de Kruijk <git@nickdekruijk.nl>
2025-12-09 16:05:59 +0100

    Add error styling for leap-pivot component
    Introduces specific styles for .leap-pivot elements with aria-invalid="true" to visually indicate errors. Updates the pivot.blade.php component to set aria-invalid and aria-errormessage attributes when validation errors are present.

--- resources/views/components/pivot.blade.php
+++ resources/views/components/pivot.blade.php
@@ -3,7 +3,8 @@
 <x-leap::label>
 </x-leap::label>
 
-<div class="leap-pivot">
+<div class="leap-pivot"
+    @error($attribute->dataName ?? ($name ?? '')) aria-errormessage="{{ $message }}" aria-invalid="true" @enderror>
     @foreach ($attribute->getValues() as $key => $value)
         <label><input type="checkbox" wire:model{{ isset($attribute->wire) ? '.' . $attribute->wire : '' }}="data.{{ $attribute->name }}" value="{{ $key }}" aria-label="{{ $value }}">{{ $value }}</label>
     @endforeach
